e13619e27a16ccc44222da4b550cb6dae85129f0
Mostefai17 <mohamediliasmostefai_etd@esgen.edu.dz>
2026-06-29 17:34:41 +0100

    setting up views and urls for the accounts app

---
+++ Youtube/accounts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from .forms import CustomUserCreationForm
+
+# Create your views here.
+
+#Writing custom regiter view
+
+class RegisterView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('accounts:login')
+    template_name = 'accounts/register.hrml'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('/')
+
+        return super().dispatch(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        login(self.request, self.object)
+        return redirect('/')
