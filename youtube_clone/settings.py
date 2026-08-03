cdf38442fd1ed48dbb9c183108575280b9d3a769
Mostefai17 <mohamediliasmostefai_etd@esgen.edu.dz>
2026-08-03 17:52:06 +0100

    feat(fixes): reconfiguring base.html and register.html routes with settings.py

--- youtube_clone/settings.py
+++ youtube_clone/settings.py
@@ -123,8 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = str(BASE_DIR / 'staticfiles')
 STATICFILES_DIRS = [
-    str(BASE_DIR / "youtube" / "static"),
+    str(BASE_DIR / "static"),
 ]
 
 LOGIN_REDIRECT_URL = '/'
